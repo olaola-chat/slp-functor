31bd0d91d0a305f5ece6b7a7d1013d7dd15ad11b
chengshuo <cheng.shuo@olaola.chat>
2023-08-21 09:35:48 +0800

    fix

--- app/api/voice_lover.go
+++ app/api/voice_lover.go
@@ -235,7 +235,7 @@ func (a *voiceLoverAPI) AudioComments(r *ghttp.Request) {
 		})
 		return
 	}
-	ret := vl_serv.VoiceLoverService.GetAudioCommentList(r.GetCtx(), req.AudioId, req.Paginator.Page, req.Paginator.Limit)
+	ret := vl_serv.VoiceLoverService.GetAudioCommentList(r.GetCtx(), req.AId, req.Paginator.Page, req.Paginator.Limit)
 	response.Output(r, ret)
 }
 
